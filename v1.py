462f2c5a8582bac7636d9618d624299983872e0b
acanacar <ahmetcanacar.1@gmail.com>
2019-04-03 22:21:37 +0300

    dataframe commission amount and commission rate is added we will have to create function for autonomous py file

--- v1.py
+++ v1.py
@@ -6,9 +6,38 @@ import seaborn as sns
 
 sns.set_style('whitegrid')
 
+
+def calc_commission_rate(x):
+    """ Return the commission rate based on the table:
+    0-90% = 2%
+    91-99% = 3%
+    >= 100 = 4%
+    """
+    if x <= .90:
+        return .02
+    if x <= .99:
+        return .03
+    else:
+        return .04
+
+
+print(calc_commission_rate.__doc__)
+
 avg = 1
 std_dev = .1
 num_reps = 500
 num_simulations = 1000
 
 pct_to_target = np.random.normal(avg, std_dev, num_reps).round(2)
+
+sales_target_values = [75000, 100000, 200000, 300000, 400000, 500000]
+sales_target_prob = [.3, .3, .2, .1, .05, .05]
+
+sales_target = np.random.choice(sales_target_values, num_reps, p=sales_target_prob)
+
+df = pd.DataFrame(index=range(num_reps), data={'Pct_To_Target': pct_to_target,
+                                               'Sales_Target': sales_target})
+
+df['Sales'] = df['Pct_To_Target'] * df['Sales_Target']
+df['Commission_Rate'] = df['Pct_To_Target'].apply(calc_commission_rate)
+df['Commission_Amount'] = df['Commission_Rate'] * df['Sales']
