6e238122e9049649a70daace602cacb463a13015
acanacar <ahmetcanacar.1@gmail.com>
2019-04-03 21:47:39 +0300

    Initial commit

---
+++ v1.py
@@ -0,0 +1,14 @@
+# Sales Representative
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+sns.set_style('whitegrid')
+
+avg = 1
+std_dev = .1
+num_reps = 500
+num_simulations = 1000
+
+pct_to_target = np.random.normal(avg, std_dev, num_reps).round(2)
